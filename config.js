b88ff826f3bb281b39d3f096c3981305fc2559da
Bassem-work <mozartmoknine@gmail.com>
2026-02-07 23:14:36 +0100

    Updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://jaonriwecvyvnmyktuys.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6Y2J2enZhd2Vib2V5YnJienp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTAyMTgsImV4cCI6MjA4NjA2NjIxOH0.mK0kYJtvspmq2gNkkQnOLxRBJS6l6wmum8t_eKKvq6U';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphb25yaXdlY3Z5dm5teWt0dXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTc5MzIsImV4cCI6MjA4NjA3MzkzMn0.V-A1xzbjubjJBg7z6076WpVyRodYLBKgI90LwiqW4PY';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
