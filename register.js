0052454844de59bf9e9c3ddf43ce85f7f3e708f9
Bassem-work <mozartmoknine@gmail.com>
2026-02-07 22:57:43 +0100

    Updated register.js

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 
   // Initialize Supabase client
   const supabaseUrl ='https://jaonriwecvyvnmyktuys.supabase.co';
-  const supabaseKey = 'YOUR_PUBLIC_ANON_KEY';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6Y2J2enZhd2Vib2V5YnJienp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTAyMTgsImV4cCI6MjA4NjA2NjIxOH0.mK0kYJtvspmq2gNkkQnOLxRBJS6l6wmum8t_eKKvq6U';
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
   document.getElementById('registerForm').addEventListener('submit', async (e) => {
