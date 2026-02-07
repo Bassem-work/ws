380edf9b6024556b3213b61944a192636e12a65c
Bassem-work <mozartmoknine@gmail.com>
2026-02-07 22:57:24 +0100

    Updated register.js

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 
   // Initialize Supabase client
-  const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
+  const supabaseUrl ='https://jaonriwecvyvnmyktuys.supabase.co';
   const supabaseKey = 'YOUR_PUBLIC_ANON_KEY';
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
