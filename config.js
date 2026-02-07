a81174e933b53769985bf7fd7c479e02ac26bfd9
Bassem-work <mozartmoknine@gmail.com>
2026-02-07 22:23:36 +0100

    Updated config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://rzcbvzvaweboeybrbzzv.supabase.co';
+const SUPABASE_URL = 'https://jaonriwecvyvnmyktuys.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6Y2J2enZhd2Vib2V5YnJienp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTAyMTgsImV4cCI6MjA4NjA2NjIxOH0.mK0kYJtvspmq2gNkkQnOLxRBJS6l6wmum8t_eKKvq6U';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
